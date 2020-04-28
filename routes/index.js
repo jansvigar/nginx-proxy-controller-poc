334b4df644cf37048ada2d9cfc3a5dd2ea174356
Jan Vigar <jansvigar@gmail.com>
2020-04-28 05:57:27 -0700

    add form to manage proxies

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
+var path = require("path");
+var fs = require("fs").promises;
+var express = require("express");
 var router = express.Router();
 
+const getProxies = async () => {
+  const fileContent = await fs.readFile(
+    path.join("/etc/nginx/nginx.conf"),
+    "utf8"
+  );
+  const found = fileContent.match(
+    /# start proxies-list([\s\S]*)# end proxies-list/i
+  );
+  let proxies;
+  if (found) {
+    proxies = found[1];
+  }
+
+  const locationRegex = /location\s*([=\^\~\* ]*\/\S*)\s*\{\s*proxy_pass\s*(\S+)[^\}]*\}/gi;
+  let matched;
+  const locations = [];
+  while ((matched = locationRegex.exec(proxies))) {
+    const obj = {
+      path: matched[1],
+      proxyUrl: matched[2],
+    };
+    locations.push(obj);
+  }
+  return locations;
+};
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", async function (req, res, next) {
+  const locations = await getProxies();
+  console.log(locations);
+  res.render("index", {
+    title: "Reverse Proxies",
+    locations,
+  });
+});
+
+router.post("/", async (req, res) => {
+  const fileContent = await fs.readFile(
+    path.join("/etc/nginx/nginx.conf"),
+    "utf8"
+  );
+  const regexToReplace = /# start proxies-list([\s\S]*)# end proxies-list/i;
+  const text = req.body.locations.map((location) => {
+    return `\t\tlocation ${location.path} {\n\t\t\tproxy_pass\t${location.proxyUrl}\n\t\t}`;
+  });
+  const replacementText = `# start proxies-list\n${text.join(
+    "\n\n"
+  )}\n\t\t# end proxies-list`;
+  const newText = fileContent.replace(regexToReplace, replacementText);
+  await fs.writeFile(path.join("/etc/nginx/nginx.conf"), newText, "utf8");
+  res.send("success!");
 });
 
 module.exports = router;
